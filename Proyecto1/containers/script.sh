6df5330a992c294770d155e7167aa500d51fe80c
C-FernandoSazo <cesarfernando.sazo@gmail.com>
2025-02-25 17:56:17 -0600

    Script creador de contenedores

--- Proyecto1/containers/script.sh
+++ Proyecto1/containers/script.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-# Lista de opciones de estrés para cada tipo de carga
-stress_commands=(
-    "stress --cpu 2 --timeout 60s"   # Alto consumo de CPU
-    "stress --vm 1 --vm-bytes 256M --timeout 60s"  # Alto consumo de RAM
-    "stress --io 2 --timeout 60s"    # Alto consumo de I/O
-    "stress --hdd 1 --hdd-bytes 500M --timeout 60s"  # Alto consumo de Disco
+# Definir las opciones de consumo
+OPTIONS=(
+    "--cpu 2 --timeout 60s"       # CPU
+    "--io 2 --timeout 60s"        # I/O
+    "--vm 2 --vm-bytes 256M --timeout 60s"  # RAM
+    "--hdd 2 --hdd-bytes 1G --timeout 60s"  # Disco
 )
 
-# Nombre de la Imagen
-image="containerstack/alpine-stress"
-
-# Crear 10 contenedores aleatorios
+# Crear 10 contenedores con nombres únicos
 for i in {1..10}; do
-    # Seleccionar aleatoriamente una de las configuraciones de estrés
-    stress_command=${stress_commands[$RANDOM % ${#stress_commands[@]}]}
+    # Seleccionar un tipo de contenedor aleatoriamente
+    RANDOM_INDEX=$((RANDOM % 4))
+    OPTION="${OPTIONS[$RANDOM_INDEX]}"
     
-    # Generar un nombre único para el contenedor
-    container_name="stress_$(date +%s)_$(head /dev/urandom | tr -dc a-z0-9 | head -c 5)"
-    
-    echo "Creando contenedor: $container_name con comando: $stress_command"
+    # Generar un nombre único basado en la fecha y /dev/urandom
+    CONTAINER_NAME="stress_$(date +%s)_$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5)"
 
-    # Ejecutar el contenedor con la configuración específica
-    docker run -d --name "$container_name" "$image" $stress_command
+    # Ejecutar el contenedor en segundo plano
+    docker run -d --name "$CONTAINER_NAME" containerstack/alpine-stress stress $OPTION
+
+    echo "Contenedor creado: $CONTAINER_NAME con opción $OPTION"
 done
