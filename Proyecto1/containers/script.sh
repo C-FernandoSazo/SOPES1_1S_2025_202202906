212e71c31349dd647d5cf3247c91ef65ec5d3b2b
C-FernandoSazo <cesarfernando.sazo@gmail.com>
2025-03-05 16:07:31 -0600

    Sh Script

--- Proyecto1/containers/script.sh
+++ Proyecto1/containers/script.sh
@@ -3,22 +3,25 @@
 # Definir las opciones de consumo
 OPTIONS=(
     "--cpu 1"       # CPU
-    "--io 1 "        # I/O
-    "--vm 2 --vm-bytes 256M "  # RAM
-    "--hdd 1 --hdd-bytes 1G "  # Disco
+    "--io 1"        # I/O
+    "--vm 2 --vm-bytes 256M"  # RAM
+    "--hdd 1 --hdd-bytes 1G"  # Disco
 )
 
-# Crear 10 contenedores con nombres únicos
+# Crear 10 contenedores con nombres únicos usando /dev/urandom
 for i in {1..8}; do
     # Seleccionar un tipo de contenedor aleatoriamente
     RANDOM_INDEX=$((RANDOM % 4))
     OPTION="${OPTIONS[$RANDOM_INDEX]}"
     
-    # Formatear la fecha y la hora en el nombre del contenedor
-    CURRENT_DATE=$(date +"%d-%m-%Y_%H%M%S")
+    # Extraer el tipo de contenedor (cpu, io, vm, hdd)
+    TYPE=$(echo "$OPTION" | awk '{print $1}' | sed 's/--//')
 
-    # Generar el nombre del contenedor con tipo de stress y fecha
-    CONTAINER_NAME="stress_${CURRENT_DATE}_${i}"
+    # Generar un ID aleatorio de 8 caracteres usando /dev/urandom
+    RANDOM_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 9)
+    
+    # Generar el nombre del contenedor con tipo y ID aleatorio
+    CONTAINER_NAME="stress_${TYPE}_${RANDOM_ID}"
 
     # Ejecutar el contenedor en segundo plano
     docker run -d --name "$CONTAINER_NAME" containerstack/alpine-stress sh -c "exec stress $OPTION"
